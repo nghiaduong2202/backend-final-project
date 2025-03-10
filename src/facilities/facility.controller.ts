a70432e28bbfc13acb9f4deeb8e7983996db8238
nghiaduong2202 <nghia.duong2202@gmail.com>
2025-03-10 22:39:35 +0700

    update create facility

--- src/facilities/facility.controller.ts
+++ src/facilities/facility.controller.ts
@@ -3,7 +3,7 @@ import {
   ClassSerializerInterceptor,
   Controller,
   Post,
-  UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
 import { AuthRoleEnum } from 'src/auths/enums/auth-role.enum';
@@ -13,9 +13,8 @@ import { AuthRoles } from 'src/auths/decorators/auth-role.decorator';
 import { ActivePeople } from 'src/auths/decorators/active-people.decorator';
 import { UUID } from 'crypto';
 import { ApiOperation } from '@nestjs/swagger';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { TestDto } from './dtos/test.dto';
-import { TestInterceptor } from './interceptors/test.interceptor';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { CreateFacilityInterceptor } from './interceptors/create-facility.interceptor';
 
 @Controller('facility')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -30,38 +29,18 @@ export class FacilityController {
   @ApiOperation({
     summary: 'create new facility and field groups and fields (role: owner)',
   })
-  @Post('')
-  @UseInterceptors(FileInterceptor('image'))
+  @Post('create')
+  @UseInterceptors(FilesInterceptor('images', 10), CreateFacilityInterceptor)
   @AuthRoles(AuthRoleEnum.OWNER)
   public create(
     @Body() createFacilityDto: CreateFacilityDto,
-    @UploadedFile() image: Express.Multer.File,
+    @UploadedFiles() images: Express.Multer.File[],
     @ActivePeople('sub') ownerId: UUID,
   ) {
-    console.log('🚀 ~ FacilityController ~ ownerId:', ownerId);
-    console.log('🚀 ~ FacilityController ~ image:', image);
-    console.log(
-      '🚀 ~ FacilityController ~ createFacilityDto:',
+    return this.facilityService.createFacility(
       createFacilityDto,
+      images,
+      ownerId,
     );
-    // return this.facilityService.createFacility(createFacilityDto, ownerId);
-  }
-
-  @ApiOperation({
-    summary: 'test get multipart file and json data',
-  })
-  @Post('/test')
-  @AuthRoles(AuthRoleEnum.NONE)
-  @UseInterceptors(FileInterceptor('image'), new TestInterceptor())
-  public test(
-    @Body('testDto') testDto: any,
-    @UploadedFile() image: Express.Multer.File,
-  ) {
-    console.log('🚀 ~ FacilityController ~ image:', image);
-    console.log('🚀 ~ FacilityController ~ testDto:', testDto);
-
-    return {
-      message: 'see in the terminal',
-    };
   }
 }
---
+++ src/facilities/interceptors/create-facility.interceptor.ts
@@ -0,0 +1,34 @@
+import {
+  BadRequestException,
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class CreateFacilityInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest<Request>();
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if (request.body.data) {
+      console.log(
+        '🚀 ~ CreateFacilityInterceptor ~ intercept ~ request.body.data:',
+        request.body.data,
+      );
+      try {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+        request.body = { ...request.body, ...JSON.parse(request.body.data) };
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        delete request.body.data;
+      } catch (error) {
+        throw new BadRequestException(error);
+      }
+    }
+
+    return next.handle();
+  }
+}
